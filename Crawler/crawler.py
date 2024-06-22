80914b8633ea7e3ea25be2aaaa5806aaab8ad309
Abeners2 <abnerlucassantoss2@gmail.com>
2024-06-22 00:06:49 -0300

    Att crawler.py

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -3,11 +3,12 @@ import requests
 from bs4 import BeautifulSoup
 
 def fetch_page(url, headers=None):
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Lança uma exceção para status HTTP diferentes de 200
         return response.content
-    else:
-        print(f'Failed to retrieve the web page. Status code: {response.status_code}')
+    except requests.exceptions.RequestException as e:
+        print(f'Failed to retrieve the web page: {e}')
         return None
 
 def crawl_website(url, headers=None):
