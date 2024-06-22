1e6fc5ed725edc26ec3be96fc6a7d275dde1669b
Abeners2 <abnerlucassantoss2@gmail.com>
2024-06-21 23:17:02 -0300

    Inserção test_crawler.py

---
+++ Crawler/tests/test_crawler.py
@@ -0,0 +1,12 @@
+# web_crawler/tests/test_crawler.py
+import unittest
+from crawler.crawler import fetch_page
+
+class TestCrawler(unittest.TestCase):
+    def test_fetch_page(self):
+        url = 'http://example.com'
+        page_content = fetch_page(url)
+        self.assertIsNotNone(page_content)
+
+if __name__ == '__main__':
+    unittest.main()
