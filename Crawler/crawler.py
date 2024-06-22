5d41e37597f4cbe75d83a39025579b819e01092a
Abeners2 <abnerlucassantoss2@gmail.com>
2024-06-22 01:19:48 -0300

    Adição run_crawler.py e modificação crawler.py

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -1,11 +1,12 @@
-# web_crawler/crawler/crawler.py
+# crawler.py
+
 import requests
 from bs4 import BeautifulSoup
 
 def fetch_page(url, headers=None):
     try:
         response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Lança uma exceção para status HTTP diferentes de 200
+        response.raise_for_status()  # Raises an exception for HTTP errors different from 200
         return response.content
     except requests.exceptions.RequestException as e:
         print(f'Failed to retrieve the web page: {e}')
@@ -15,5 +16,25 @@ def crawl_website(url, headers=None):
     page_content = fetch_page(url, headers)
     if page_content:
         soup = BeautifulSoup(page_content, 'html.parser')
+        
+        # Extract page title
+        page_title = soup.title.string.strip() if soup.title else 'No title found'
+        print(f'Title of the page: {page_title}')
+        
+        # Extract all links
+        links = [link.get('href') for link in soup.find_all('a') if link.get('href')]
+        print(f'Links found ({len(links)}):')
+        for link in links:
+            print(link)
+        
+        # Extract all images
+        images = [img.get('src') for img in soup.find_all('img') if img.get('src')]
+        print(f'Images found ({len(images)}):')
+        for img in images:
+            print(img)
+
+        # You can add more extraction logic here for other elements like text, metadata, etc.
+
         return soup
+
     return None
